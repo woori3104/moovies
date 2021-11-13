f70258d52a288f37137bd9ce0ae1091fb0bbe118
woori3104 <sizuku11261@gmail.com>
2021-11-14 00:12:31 +0900

    AppLoading part Three

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
-import AppLoading from "expo-app-loading";
-import React, { useState } from "react";
 import * as Font from "expo-font";
 import { Text, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { Asset } from "expo-asset";
+import { Asset, useAssets } from "expo-asset";
+
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      // 웹상의 이미지를 가져옴
+      return Image.prefetch(image);
+    } else {
+      // 로컬의 이미지를 가져옴
+      return Asset.loadAsync(image);
+    }
+  });
 
 export default function App() {
+  const [assets] = useAssets([require("./my-face.jpeg")]);
+  const [loaded] = Font.useFonts(Ionicons.font);
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async () => {
-    await Font.loadAsync(Ionicons.font);
-    // 로컬에서 사진파일을 가져옴
-    await Asset.loadAsync(require("./landers.jpeg"));
-    // 웹에서 사진 파일을 가져옴
-    await Image.prefetch("https://reactnative.dev/img/oss_logo.png");
+    const fonts = loadFonts([Ionicons.font]);
+    const images = loadImages([
+      require("./my-face.jpeg"),
+      // 문자열을 보내줄경우 -> prefetch
+      "https://reactnative.dev/img/oss_logo.png",
+    ]);
+    await Promise.all([...fonts, ...images]);
   };
   if (!ready) {
     return (
