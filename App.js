fec78a2d89b4eb0ee90c45e3dd6a00e711c910bf
woori3104 <sizuku11261@gmail.com>
2021-11-13 23:59:27 +0900

    AppLoading part Two

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import AppLoading from "expo-app-loading";
-import React , {useState} from 'react';
-import { Text } from "react-native"
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import { Text, Image } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
 
 export default function App() {
   const [ready, setReady] = useState(false);
-  // onFinish는 로딩이 끝나면 호출되는 함수
   const onFinish = () => setReady(true);
-
-  const startLoading = async () => {};
+  const startLoading = async () => {
+    await Font.loadAsync(Ionicons.font);
+    // 로컬에서 사진파일을 가져옴
+    await Asset.loadAsync(require("./landers.jpeg"));
+    // 웹에서 사진 파일을 가져옴
+    await Image.prefetch("https://reactnative.dev/img/oss_logo.png");
+  };
   if (!ready) {
     return (
       // AppLoading을 렌더링할 때까지 splash screen이 계속 떠있게함
