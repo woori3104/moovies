c0b96add2cd517520606350515bc03b5f35e7eb6
woori3104 <sizuku11261@gmail.com>
2021-11-16 22:45:43 +0900

    Dark Mode

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,29 +3,36 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
-import { Text, View } from "react-native";
+import { useColorScheme } from "react-native";
+import { BLACK_COLOR, DARK_GREY, LIGHT_GREY, YELLOW_COLOR } from "../colors";
 
 const Tab = createBottomTabNavigator();
 
 // name을 변경하면 하단 탭이름과 타이틀 이름이 변경됨 
 // screenOptions을 이용하여 Navigation의 색상등의 설정을 변경할 수 있다. 
-const Tabs = () => (
-  <Tab.Navigator
-    screenOptions={{
-      tabBarStyle: { backgroundColor: "tomato" },
-      tabBarActiveTintColor: "red",
-      tabBarInactiveTintColor: "purple",
-      headerTitleStyle: { color: "tomato" },
-      headerRight: () => (
-        <View>
-          <Text>Hello</Text>
-        </View>
-      ),
-    }}
-  >
-    <Tab.Screen name="Movies" component={Movies} />
-    <Tab.Screen name="Tv" component={Tv} />
-    <Tab.Screen name="Search" component={Search} />
-  </Tab.Navigator>
-);
+// colorScheme을 사용하면 다크모드로 손쉽게 변경 가능 
+const Tabs = () => {
+  const isDark = useColorScheme() === "dark";
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: isDark ? BLACK_COLOR : "white",
+        },
+        tabBarActiveTintColor: isDark ? YELLOW_COLOR : BLACK_COLOR,
+        tabBarInactiveTintColor: isDark ? DARK_GREY : LIGHT_GREY,
+        headerStyle: {
+          backgroundColor: isDark ? BLACK_COLOR : "white",
+        },
+        headerTitleStyle: {
+          color: isDark ? "white" : BLACK_COLOR,
+        },
+      }}
+    >
+      <Tab.Screen name="Movies" component={Movies} />
+      <Tab.Screen name="Tv" component={Tv} />
+      <Tab.Screen name="Search" component={Search} />
+    </Tab.Navigator>
+  );
+};
 export default Tabs;
