6bef9b7de6e8353e7a852084df3a49c1ca748d4d
woori3104 <sizuku11261@gmail.com>
2021-11-28 15:18:19 +0900

    refactoring

--- api.ts
+++ api.ts
@@ -1,6 +1,33 @@
 const API_KEY = "8357ede894421639d86219a3f5fcae0d";
 const BASE_URL = "https://api.themoviedb.org/3";
 
+export interface Movie {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface BaseResponse {
+  page: number;
+  total_results: number;
+  total_pages: number;
+}
+
+export interface MovieResponse extends BaseResponse {
+  results: Movie[];
+}
+
 const trending = () =>
   fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`).then((res) =>
     res.json()
@@ -10,6 +37,7 @@ const upcoming = () =>
   fetch(
     `${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
   );
+
 const nowPlaying = () =>
   fetch(
     `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`
